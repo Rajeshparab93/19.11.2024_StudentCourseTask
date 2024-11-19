55e0bba8142ec4d1e335b77d9553df72f924bfff
ADMS JAVA GRP01 <adms.java1@CTPL.in>
2024-11-19 17:43:32 +0530

    [STNO-3]Second Commit

--- StudentCources1/src/main/java/com/clover/service/StudentService.java
+++ StudentCources1/src/main/java/com/clover/service/StudentService.java
@@ -22,24 +22,24 @@ public class StudentService {
 	    Course course = courseRepo.findById(courseId).orElse(null);
 
 	    if (student == null || course == null) {
-	        return null; // Return null if student or course not found
+	        return null; 
 	    }
 
-	    student.getCourses().add(course); // Add course to student's course list
-	    return studentRepo.save(student); // Save updated student
-	  //  return student; // Return updated student
+	    student.getCourses().add(course); 
+	    return studentRepo.save(student); 
+	  
 	}
 	
 	public Student getStudentCourses(Long studentId) {
 	    return studentRepo.findById(studentId).orElse(null);
 	}
 	
-	  // Create a new student
+	  
     public Student createStudent(Student student) {
         return studentRepo.save(student);
     }
 
-    // Create a new course
+   
     public Course createCourse(Course course) {
         return courseRepo.save(course);
     }
